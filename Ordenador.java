35b49f706201c0ddc3d3705add4a46b5b4ebcf61
Miguel Redondo <11308sch@gmail.com>
2023-11-14 11:10:27 +0100

    Añadir atributos clases ordenador

--- Ordenador.java
+++ Ordenador.java
@@ -1,2 +1,8 @@
 public class Ordenador {
+    // Atributo para la marca
+    private String marca;
+    // Atributo para frecuenciaEnGhz
+    private boolean estadoEncendido;
+    // Atributo para procesador
+    private Procesador procesador;
 }
