4e235a01557232c3652d6a3625a2a8ae0ce107e9
Miguel Redondo <11308sch@gmail.com>
2023-11-14 11:15:08 +0100

    Añadidos metodos getter de la clase Ordenador

--- Ordenador.java
+++ Ordenador.java
@@ -1,17 +1,26 @@
 public class Ordenador {
-<<<<<<< HEAD
     // Atributo para la marca
     private String marca;
     // Atributo para frecuenciaEnGhz
     private boolean estadoEncendido;
     // Atributo para procesador
     private Procesador procesador;
-}
-=======
+
     public Ordenador(String marca, boolean estaEncendido, Procesador procesador) {
         this.marca = marca;
         this.estaEncendido = estaEncendido;
         this.procesador = procesador;
     }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public String GetEstadoEncendido(){
+        return estadoEncendido;
+    }
+
+    public String getProcesador(){
+        return procesador;
+    }
 }
->>>>>>> 545f7d6284cdd7448bd2c17253f317bec7ea18e7
