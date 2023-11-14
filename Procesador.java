4ce474dd35f293aca9f86de9fd8ad7e5fdabb32c
kinire98 <iknitgarri@outlook.com>
2023-11-14 10:56:39 +0100

    Añadidos los atributos y el constructor de la clase Procesador

--- Procesador.java
+++ Procesador.java
@@ -1,2 +1,11 @@
 public class Procesador {
+    // Atributo para la marca
+    private String marca;
+    // Atributo para frecuenciaEnGhz
+    private double frecuenciaEnGhz;
+
+    public Procesador(String marca, double frecuenciaEnGhz) {
+        this.marca = marca;
+        this.frecuenciaEnGhz = frecuenciaEnGhz;
+    }
 }
