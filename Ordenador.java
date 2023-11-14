c2b14dbb6b2674d73466ac99b93ae13c2610db9d
kinire98 <iknitgarri@outlook.com>
2023-11-14 11:12:07 +0100

    Cambiado el nombre del atributo

--- Ordenador.java
+++ Ordenador.java
@@ -1,17 +1,14 @@
 public class Ordenador {
-<<<<<<< HEAD
     // Atributo para la marca
     private String marca;
     // Atributo para frecuenciaEnGhz
-    private boolean estadoEncendido;
+    private boolean estaEncendido;
     // Atributo para procesador
     private Procesador procesador;
-}
-=======
+
     public Ordenador(String marca, boolean estaEncendido, Procesador procesador) {
         this.marca = marca;
         this.estaEncendido = estaEncendido;
         this.procesador = procesador;
     }
 }
->>>>>>> 545f7d6284cdd7448bd2c17253f317bec7ea18e7
